bbc21fdbde8742edb2f95a963245d2bb3057e456
danielkim3 <wjinkim21@naver.com>
2021-12-21 14:01:20 +0530

    state변경

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+/*eslint-disable */
+
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
+  
   let [VSCode, a] = useState(['VS code 필수 단축키!', 'Intelli J 단축키!']);
   let [React, b] = useState('React.js 하는방법!');
   let [CHash, c] = useState('정처기를 따려면 C#은 필수?');  
-  
+  let [따봉, 따봉변경] = useState(0);
   let style = {color : 'white', fontSize : '30px'}
   let imgStyle ={width: '60px'}
 
+  function 타이틀변경(){
+    var newArray = [...VSCode];
+    newArray[0] = 'Intelli J 단축키!';
+    a(newArray);
+  }
+
+  
   return (
     <div className="App">
       <div className="black-nav">
         <img  style ={imgStyle}  src={logo}/>
-        <div style ={style}>개발 blog</div>       
-        
-      </div>          
+        <div style ={style}>개발 blog</div>         
+      </div>    
+      <button onClick ={타이틀변경}>버튼</button>      
       <div className="list">
-        <h3>{ VSCode[0] }</h3>
+        <h3>{ VSCode[0] }<span onClick={() => {따봉변경(따봉 + 1)}}>👍</span>{따봉}</h3>
         <p>12월 08일 발행</p>
         <hr/>    
       </div>
